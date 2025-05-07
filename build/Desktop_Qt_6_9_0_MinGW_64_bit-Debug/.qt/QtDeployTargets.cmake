3167a8d29cc546f4161c65dab1afd83d067d3afd
dyc1111 <dingjie2002@gmail.com>
2025-05-07 16:50:19 +0800

    Delete build

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_test_project_FILE C:/Users/32880/Documents/GitHub/unnamed/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/test_project.exe)
-set(__QT_DEPLOY_TARGET_test_project_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_test_project_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
