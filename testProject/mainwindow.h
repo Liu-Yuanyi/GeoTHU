2ecda35c8ed30d8958a219623fbe528a77f100e0
dyc1111 <dingjie2002@gmail.com>
2025-05-07 08:31:20 +0800

    Add files via upload

---
+++ testProject/mainwindow.h
@@ -0,0 +1,21 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <QToolBar>
+
+class Canvas; // Forward declaration
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+
+private:
+    QPushButton* btnSelect;
+    QPushButton* btnCreate;
+    Canvas* canvas;
+};
+
+#endif // MAINWINDOW_H
